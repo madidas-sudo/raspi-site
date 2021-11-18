78c2274265b72dcf27a42b4c30f89316b45d3fa8
madidas-sudo <65229348+madidas-sudo@users.noreply.github.com>
2021-11-18 20:17:44 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,8 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(0);
-    noStroke();
-    fill(255);
-    textSize(32);
-    textAlign(CENTER);
-    text("Click to start", width/2, height/2);
+    createCanvas(windowInnerWidth, windowInnerHeight);
 }
 
 function draw() {
-    if (mouseIsPressed) {
-        background(0);
-        fill(255);
-        text("Click to stop", width/2, height/2);
-    }
+    background(52);
+    ellipse(mouseX, mouseY, 80, 80);
 }
